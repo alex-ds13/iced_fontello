ee52770bfa00b07012430e2852f9aaa600d9267b
alex-ds13 <145657253+alex-ds13@users.noreply.github.com>
2024-12-12 17:51:26 +0000

    fix: remove wrong iced icon and add cargo reruns
    - Removes the wrong iced icon "iced_logo".
    - Add `cargo::rerun-if-changed` lines for each svg file, since it
      doesn't accept wildcards, using the directory still triggers a build
      on any change. So the only way to make it work correctly is to add a
      line for each svg file. If the user doesn't intend to change the svg
      files without changing the TOML file as well then these lines for each
      svg can be omitted.

--- example/src/icon.rs
+++ example/src/icon.rs
@@ -1,6 +1,6 @@
 // Generated automatically by iced_fontello at build time.
 // Do not edit manually. Source: ../fonts/example-icons.toml
-// 062ad2a2c43d8a1842f6a0449f6bd0acadcfe91c2c6b40ae2c7e1c7e1163d9c9
+// 1977ed694f963366e3d65fb47eda9896c38d2b6d11620a0c29c91ce41a31f9c0
 use iced::widget::{Text, text};
 
 pub const FONT: &[u8] = include_bytes!("../fonts/example-icons.ttf");
@@ -13,12 +13,8 @@ pub fn iced<'a>() -> Text<'a> {
     icon("\u{E800}")
 }
 
-pub fn iced_logo<'a>() -> Text<'a> {
-    icon("\u{E801}")
-}
-
 pub fn rust<'a>() -> Text<'a> {
-    icon("\u{E802}")
+    icon("\u{E801}")
 }
 
 pub fn save<'a>() -> Text<'a> {
